daf56ed1df353a0a3d9c62bf173bd83a5e0be68b
Jennifer Vargas <jevargasv1990@gmail.com>
2018-09-16 19:38:56 -0400

    Finished and polished everything!

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,8 +1,16 @@
-let closeButton = document.querySelector(".close-button")
-let flashPanel = document.querySelector(".flash")
+let closeButton = document.querySelector(".close-button");
+let flashPanel = document.querySelector(".flash");
 
 closeButton.addEventListener("click", event => {
   event.preventDefault()
 
   flashPanel.classList.add("hide")
-})
+});
+
+function overStyle(object) {
+  object.style.color = 'white';
+}
+
+function outStyle(object) {
+  object.style.color = 'white';
+}
